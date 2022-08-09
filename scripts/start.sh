bd6d0ee16ab6c8d5212f7eb5714680ac7953a092
sohee kim <sohee063@nate.com>
2022-08-09 13:48:09 +0900

    파이프라인 구축 실습5

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 아래 위치는 여러분이 해당 리포지토리를 클론한 위치로 설정해주세요.
-cd /Users/sohee/Desktop/fe-sprint-practice-deploy/server
+cd Desktop/fe-sprint-practice-deploy/server
 authbind --deep pm2 start app.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 아래 위치는 여러분이 해당 리포지토리를 클론한 위치로 설정해주세요.
-cd /Users/sohee/Desktop/fe-sprint-practice-deploy/server
+cd Desktop/fe-sprint-practice-deploy/server
 pm2 stop app.js 2> /dev/null || true
 pm2 delete app.js 2> /dev/null || true
